a6990866de9638b3b7a5e8936b0d1b6c903eee89
Jose Mejia <mejialaguna@yahoo.com>
2021-07-20 22:50:55 -0400

    time manage

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,26 +1,28 @@
     
 var container = $(".container");
-var currentDay = $("#currentDay");
-var convertedDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-currentDay.text(convertedDate);
-var allTask = []
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+// var newTask = JSON.parse(localStorage.getItem("newTask")) || [];
 const btnSave = $(".btn-save");
-const task = $("#task");
+// var userFormEl = document.querySelector("#user-form");
 
 
 
 
-$(".saveBtn").click(function() {
+$(".saveBtn").click(function(event) {
+    // preventDefault refresh after clicking the save btn
+    event.preventDefault();
+
     var thisBtn = $(this)
     var hour = thisBtn.siblings("span").text()
     var task = thisBtn.siblings("textarea").val()   // value insted of text
     var newTask = {
         hour : hour , 
         textTask : task,
-    } 
+    }
+
     if (!newTask) {
         alert("You need to fill out the task form!");
-        return false;
+        // return false;
       }
 
       
@@ -31,11 +33,29 @@ $(".saveBtn").click(function() {
     // if (
         
     // )
-    localStorage.setItem('newTask', JSON.stringify(newTask));
+    localStorage.setItem('newTask', JSON.stringify(newTask));     
     localStorage.getItem('newTask', JSON.stringify(newTask));
 
 });
 
+// function to check the due time during the day
+function Color() {
+    $(".color").each(function () {
+        let taskTime = $(".time").val();
+        let currentTime = (moment().format("h"));
+        if (taskTime > currentTime) {
+            $(this).addClass("future")
+        } else if (taskTime == currentTime) {
+            $(this).addClass("present")
+        } else {
+            $(this).addClass("past")
+        }
+    });
+}
+// interval to check the time 
+setInterval(function () {
+    Colors()
+}, (1000*60)*60*24)
 
-
-
+// running the function color 
+Color()
